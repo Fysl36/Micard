cf2e721b488966cc1697ca0449c73eba98bd4132
fysl3 <fysl36@gmail.com>
2023-08-15 16:13:19 +0600

    commit successfully

--- lib/main.dart
+++ lib/main.dart
@@ -11,85 +11,84 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.black,
+        backgroundColor: Colors.blue,
         body: SafeArea(
-            child: Column(
-          children: <Widget>[
-            CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('images/Fayasal.jpg'),
-            ),
-            Text(
-              'Faysal Mahmud',
-              style: TextStyle(
-                fontFamily: 'RubikWetPaint',
-                fontSize: 40.0,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/Fayasal.jpg'),
+              ),
+              Text(
+                'Faysal Mahmud',
+                style: TextStyle(
+                  fontFamily: 'TiltPrism-Regular-VariableFont_XROT,YROT',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                ''
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'Roboto-Black',
+                  color: Colors.teal.shade100,
+                  fontSize: 20.0,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  color: Colors.amberAccent,
+                ),
+              ),
+              Card(
                 color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Text(
-              ''
-              'FLUTTER DEVELOPER',
-              style: TextStyle(
-                fontFamily: 'TiroBangla',
-                color: Colors.teal.shade100,
-                fontSize: 20.0,
-                letterSpacing: 2.5,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(
-                vertical: 10.0,
-                horizontal: 25.0,
-              ),
-              child: Row(
-                children: <Widget>[
-                  Icon(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10.0,
+                  horizontal: 25.0,
+                ),
+                child: ListTile(
+                  leading: Icon(
                     Icons.phone,
-                    color: Colors.blueAccent,
+                    color: Colors.purple,
                   ),
-                  SizedBox(
-                    width: 30.0,
-                  ),
-                  Text(
+                  title: Text(
                     '01766302556',
                     style: TextStyle(
-                      color: Colors.blue,
-                      fontFamily: 'TiroBangla',
                       fontSize: 20.0,
+                      fontFamily: 'TiroBangla',
+                      color: Colors.blueGrey,
                     ),
                   ),
-                ],
+                ),
               ),
-            ),
-            Card(
-              color: Colors.white,
-              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-              child: Row(
-                children: [
-                  Icon(
+              Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
                     Icons.email,
-                    color: Colors.blueAccent,
+                    color: Colors.purple,
                   ),
-                  SizedBox(
-                    width: 30.0,
-                  ),
-                  Text(
+                  title: Text(
                     'fysl36@gmail.com',
                     style: TextStyle(
                       fontSize: 20.0,
                       fontFamily: 'TiroBangla',
-                      color: Colors.blueAccent,
+                      color: Colors.blueGrey,
                     ),
-                  )
-                ],
+                  ),
+                ),
               ),
-            )
-          ],
-        )),
+            ],
+          ),
+        ),
       ),
     );
   }
